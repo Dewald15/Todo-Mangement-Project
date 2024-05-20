fed5b3b1ea994b42a5a7e4031d618918e5806058
Dewald15 <vdbergdewald@gmail.com>
2024-05-20 10:01:23 +0200

    Created Add Todo REST API

---
+++ src/main/java/net/javaguides/todo/service/impl/TodoServiceImpl.java
@@ -0,0 +1,32 @@
+package net.javaguides.todo.service.impl;
+
+import lombok.AllArgsConstructor;
+import net.javaguides.todo.dto.TodoDto;
+import net.javaguides.todo.entity.Todo;
+import net.javaguides.todo.repository.TodoRepository;
+import net.javaguides.todo.service.TodoService;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class TodoServiceImpl implements TodoService {
+
+    private TodoRepository todoRepository;
+    @Override
+    public TodoDto addTodo(TodoDto todoDto) {
+        Todo todo = new Todo();
+        todo.setTitle(todoDto.getTitle());
+        todo.setDescription(todoDto.getDescription());
+        todo.setCompleted(todoDto.isCompleted());
+
+        Todo savedTodo = todoRepository.save(todo);
+
+        TodoDto savedTodoDto = new TodoDto();
+        savedTodoDto.setId(savedTodo.getId());
+        savedTodoDto.setTitle(savedTodo.getTitle());
+        savedTodoDto.setDescription(savedTodo.getDescription());
+        savedTodoDto.setCompleted(savedTodo.isCompleted());
+
+        return savedTodoDto;
+    }
+}
