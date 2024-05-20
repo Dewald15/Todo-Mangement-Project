ed540a916ddf8c2871899803f8dd2442d23f894f
Dewald15 <vdbergdewald@gmail.com>
2024-05-20 11:52:29 +0200

    Implemented Get All Todo REST API

--- src/main/java/net/javaguides/todo/service/TodoService.java
+++ src/main/java/net/javaguides/todo/service/TodoService.java
@@ -2,7 +2,11 @@ package net.javaguides.todo.service;
 
 import net.javaguides.todo.dto.TodoDto;
 
+import java.util.List;
+
 public interface TodoService {
     TodoDto addTodo(TodoDto todoDto);
     TodoDto getTodo(Long id);
+
+    List<TodoDto> getAllTodos();
 }
